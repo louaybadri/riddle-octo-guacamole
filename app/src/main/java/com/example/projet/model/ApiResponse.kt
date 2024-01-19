69f52ccff814130a015e775739fd83505543478d
louay <louay.badri@insat.ucar.tn>
2024-01-19 18:03:54 +0100

    first commit

---
+++ app/src/main/java/com/example/projet/model/ApiResponse.kt
@@ -0,0 +1,5 @@
+package com.example.projet.model
+
+data class ApiResponse(
+    val `data`: List<Plant>,
+)
---
+++ app/src/main/java/com/example/projet/model/Plant.kt
@@ -0,0 +1,7 @@
+package com.example.projet.model
+
+data class Plant(
+    val id: Int,
+    val image_url: String,
+    val common_name: String,
+    )
