4f41f4d37ce4f8683168e73b68c60fd3125d7a49
louay <louay.badri@insat.ucar.tn>
2024-01-20 17:03:20 +0100

    internet checking done

--- app/src/main/java/com/example/projet/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/projet/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.projet.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +11,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.projet.databinding.FragmentHomeBinding
-import com.example.projet.model.Plant
+import com.example.projet.dummy_data.DUMMY
+import com.example.projet.ui.utils.ConnectivityUtils
 
 class HomeFragment : Fragment() {
 
@@ -20,56 +22,43 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    val fakeUrl = "https://www.codelikethewind.org/content/images/size/w2000/2022/05/hello_world.png"
 
-    private val plantList = arrayListOf<Plant>(
-        Plant(1, fakeUrl, "Plant One"),
-        Plant(2, fakeUrl, "Plant Two"),
-        Plant(1, fakeUrl, "Plant One"),
-        Plant(2, fakeUrl, "Plant Two"),
-        Plant(1, fakeUrl, "Plant One"),
-        Plant(2, fakeUrl, "Plant Two"),
-        Plant(1, fakeUrl, "Plant One"),
-        Plant(2, fakeUrl, "Plant Two"),
-        Plant(1, fakeUrl, "Plant One"),
-        Plant(2, fakeUrl, "Plant Two"),
-        Plant(1, fakeUrl, "Plant One"),
-        Plant(2, fakeUrl, "Plant Two"),
-        // Add more plant instances as needed
-    )
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-        val welcomeTextView: TextView = binding.textHome
-        val recyclerView: RecyclerView = binding.recyclerView
-
-        // Set content or handle actions as needed
-        welcomeTextView.text = "Welcome to the Riddle App!"
-
-        // Set up RecyclerView
-        val layoutManager = LinearLayoutManager(activity)
-        recyclerView.layoutManager = layoutManager
-
-        val adapter = PlantAdapter(plantList)
-        recyclerView.adapter = adapter
+        refresh(5000);
 
         return root
     }
-
-
-
+    private val handler = Handler()
+    private fun refresh(millisecton: Long) {
+        val runnable = Runnable {
+            if (ConnectivityUtils.isInternetAvailable(requireContext())) {
+                // DO SOMETHING HERE
+                val welcomeTextView: TextView = binding.textHome
+                welcomeTextView.text = "Welcome to the Riddle App!"
+                val recyclerView: RecyclerView = binding.recyclerView
+                // Set up RecyclerView
+                val layoutManager = LinearLayoutManager(activity)
+                recyclerView.layoutManager = layoutManager
+                val adapter = PlantAdapter(DUMMY.plantList)
+                recyclerView.adapter = adapter
+            } else {
+                ConnectivityUtils.handleNoInternetConnection(requireContext())
+                handler.postDelayed({ refresh(millisecton) }, millisecton)
+            }
+        }
+ runnable.run()}
 
 
     override fun onDestroyView() {
